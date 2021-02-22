2cb9274768017d1937855a6032ba5975ae78e797
currymike <currymike@gmail.com>
2021-02-22 18:34:10 -0500

    Customize Tracker page

--- client/src/views/Tracker.vue
+++ client/src/views/Tracker.vue
@@ -1,9 +1,39 @@
 <template>
 <div class="container">
-  <div class="notification is-primary">
-    This container is <strong>centered</strong> on desktop and larger viewports.
+  <div class="columns">
+    <div class="column is-one-third">
+
+       <aside class="menu">
+
+        <p class="menu-label">
+           Carido
+        </p>
+
+         <ul class="menu-list">
+           <li><a>Tracking</a></li>
+           <li><a>Past Runs</a></li>
+           </ul>   
+
+        </aside>
+    </div>
+
+    <div class="column is-two-thirds"><div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="http://ontheworldmap.com/usa/state/new-york/map-of-new-york.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+   <h1>Starting Location</h1>
+
+   
   </div>
 </div>
+
+    </div>
+
+    </div>
+  </div>
                 
 </template>
 
