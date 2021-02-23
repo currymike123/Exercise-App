c8bbffd871ef12fafab4610176ffe7715e2da302
currymike <currymike@gmail.com>
2021-02-22 19:12:04 -0500

    Updated Title Sequence

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
+
+ 
+
+
  <div class="container">
+
+  <div class="columns level-item">
+   <div class="column is-one-half">
+    <h1 class="subtitle is-1 ">
+      <strong>Will</strong>
+         
+       :gives you <strong>strength</strong><br> to keep going all day.
+    </h1>
+   
+   </div>
+   <div class="column">
+   </div>
+  </div>
+
   <div class="notification is-success level-item">
     <i class="fas fa-dumbbell fa-10x"></i>
   </div>
 </div>
+
+
+
 </template>
 
 <script>
