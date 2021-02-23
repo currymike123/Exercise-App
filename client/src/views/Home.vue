9f70b9bc51f532424b465f4f017e257cfff9217b
currymike <currymike@gmail.com>
2021-02-22 22:48:46 -0500

    Updated margin in Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,8 +5,8 @@
 
  <div class="container">
 
-  <div class="columns level-item">
-   <div class="column is-one-half">
+  <div class="columns level-item m-6">
+   <div class="column is-one-half m-6">
     <h1 class="subtitle is-1 ">
       <strong>Will</strong>
          
@@ -14,7 +14,7 @@
     </h1>
    
    </div>
-   <div class="column">
+   <div class="column m-6">
      This is the write up about the app and all the wonderful things it does to keep you fit and strong.  Here I'll put links and other descriptions of the project and app.
    </div>
   </div>
