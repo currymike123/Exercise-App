dce532fcb6deba9a6fa9026401b00035b4e06b93
currymike <currymike@gmail.com>
2021-03-04 09:39:14 -0500

    Heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 /* Server Code */
-
+/*
 const express = require('express')
 const app = express()
 const port = 3000
@@ -14,3 +14,22 @@ app
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+*/
+
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '../client/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
