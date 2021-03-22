15ddf8af7a277e8bc50ec25b28a93e41054fdc80
currymike <currymike@gmail.com>
2021-03-21 22:17:52 -0400

    quick friends change

--- server/models/friends.js
+++ server/models/friends.js
@@ -2,9 +2,10 @@
 
 const users = require("./users");
 
+//Relation is user_handle.  Updatet friends array with new friends.
 const list = [
   {
-    friends: [],
+    friends: ["@mikecurry"],
     user_handle: "@johnsmith",
   },
 ];
@@ -15,6 +16,8 @@ module.exports.GetAll = () =>
     user: users.GetByhandle(x.user_handle),
   }));
 module.exports.Get = (friend_id) => list[friend_id];
+
+//Add to friends list for only that user_handle
 module.exports.Add = (friend) => {
   if (!friend.user_handle) {
     throw "friend must have an Owner";
@@ -22,12 +25,15 @@ module.exports.Add = (friend) => {
   list.push(friend);
   return { ...friend };
 };
+
+//Same as post?
 module.exports.Update = (friend_id, friend) => {
   const oldObj = list[friend_id];
   const newObj = { ...oldObj, ...friend };
   list[friend_id] = newObj;
   return newObj;
 };
+
 module.exports.Delete = (friend_id) => {
   const friend = list[friend_id];
   list.splice(friend_id, 1);
